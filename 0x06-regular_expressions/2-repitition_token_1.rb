1ae815613e25d3a9f68ca67e996dc1e504bb7e77
Ariole David <davidariole@gmail.com>
2021-11-06 16:53:43 +0100

    Task 1 - 7

---
+++ 0x06-regular_expressions/2-repitition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# This  0x06. Regular expression matchesvarious cases 
+puts ARGV[0].scan(/h.?tn/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# This  0x06. Regular expression matches various cases 
+puts ARGV[0].scan(/[A-Z]/).join
